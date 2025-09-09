480ba256c5328ea8c92c0f4aa3aa950720b8917d
albazeraw <albanosuarez739@gmail.com>
2025-09-08 21:14:51 -0300

    agregado proyecto dominio y clase articulos

---
+++ dominio/articulos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class articulos
+    {
+        public int idArticulo{get; set;}
+        public string nombre{get; set;} 
+        public string descripcion{ get; set;}
+        public decimal precio { get; set; }
+        public marcas marca { get; set; }
+        public categorias categoria { get; set; }
+
+    }
+}
